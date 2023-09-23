c352325e75c5bb52d5b6ea090e44c68876c59911
João Marcos <114954938+marcosilva2336@users.noreply.github.com>
2023-09-23 17:08:04 -0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -6,13 +6,7 @@ import { getFunctions } from 'firebase/functions';
 import { serverTimestamp } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAMxWOPGLwX-ZV-3qA5c6XiNP7NrCFMpJk",
-    authDomain: "instagram-clone-28abd.firebaseapp.com",
-    projectId: "instagram-clone-28abd",
-    storageBucket: "instagram-clone-28abd.appspot.com",
-    messagingSenderId: "332347118791",
-    appId: "1:332347118791:web:03f5f4f06ca1a20ededae2",
-    measurementId: "G-X0KDR19SLH"
+  
 };
 
 const app = initializeApp(firebaseConfig);
